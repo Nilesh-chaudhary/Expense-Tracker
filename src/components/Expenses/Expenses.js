b66f0d3c55eb05302f1fa974fb6dc8b7f1de5d86
Nilesh-chaudhary <nileshchaudhary532@gmail.com>
2022-10-17 23:30:14 +0530

    Adding to git after a long time

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+
+import ExpensesList from "./ExpensesList";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {/* {expensesContent} */}
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
+
+// {
+//   /* {filteredExpenses.length === 0 && <p>No Expenses found.</p>}   // this also works but above is another technique
+//         {filteredExpenses.length > 0 &&
+//           filteredExpenses.map((expense) => (
+//             <ExpenseItem
+//               key={expense.id} // this will add item directly to the top without affecting others
+//               title={expense.title}
+//               amount={expense.amount}
+//               date={expense.date}
+//             />
+//           ))} */
+// }
+// {
+//   /*
+//         // old way static
+//         <ExpenseItem
+//           title={props.items[0].title}
+//           amount={props.items[0].amount}
+//           date={props.items[0].date}
+//         />
+//         <ExpenseItem
+//           title={props.items[1].title}
+//           amount={props.items[1].amount}
+//           date={props.items[1].date}
+//         />
+//         <ExpenseItem
+//           title={props.items[2].title}
+//           amount={props.items[2].amount}
+//           date={props.items[2].date}
+//         />
+//         <ExpenseItem
+//           title={props.items[3].title}
+//           amount={props.items[3].amount}
+//           date={props.items[3].date}
+//         /> */
+// }
